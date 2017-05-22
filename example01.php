4db00f118f0c9fd791fe3bb997d1e41e090d84fd
Sammy Kaye Powers <sammyk@sammykmedia.com>
2017-05-22 17:13:16 -0500

    Update main example

--- example01.php
+++ example01.php
@@ -1,7 +1,14 @@
 <?php
 
-function makeAwesome(string $a): string {
-    return sprintf('Cannot make %s awesome b/c they already are!', $a);
+if ('cli' !== PHP_SAPI) {
+    header('Content-type: text/plain;');
+}
+
+function whoIs(string $a): string {
+    if ('Derick Rethans' === $a) {
+        return sprintf('%s is the creator of xdebug.', $a);
+    }
+    return sprintf('%s is an awesome internals contributor.', $a);
 }
 
 $phpeople = [
@@ -12,6 +19,8 @@ $phpeople = [
 ];
 
 foreach ($phpeople as $nerd) {
-    $message = makeAwesome($nerd);
-    echo $message . PHP_EOL;
+    $message = whoIs($nerd);
+    echo $message . "\n";
 }
+
+echo "Done :)\n";
